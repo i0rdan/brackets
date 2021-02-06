3c02c49a5a825d9e65f82fa75fa1564f935f18ea
Evgeny Kazusev <55188582+i0rdan@users.noreply.github.com>
2021-02-06 21:30:03 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let x = 0; x < str.length; x++) {
+    for (let y = 0; y < bracketsConfig.length; y++) {
+      if(str[x] == bracketsConfig[y][0] && str[x + 1] == bracketsConfig[y][1]) {
+        let z = bracketsConfig[y][0] + bracketsConfig[y][1];
+		str = str.replace(z, '');
+		x -= 2;
+      }
+    }
+  }
+  return str == '';
 }
